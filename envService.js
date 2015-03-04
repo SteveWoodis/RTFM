eac25f02fa18fe0819d354386c75f0557ff7bdba
Steve <swoodis2010@gmail.com>
2015-03-03 19:40:11 -0700

    making a backup for protection

--- envService.js
+++ envService.js
@@ -1,25 +1,39 @@
-angular.module('rtfmApp').service('envService', function envService($window) {
+angular.module('rtfmApp').service('envService', function envService() {
     
-      this.getEnv= function () {
-        return $window.env;
-      }
+//      this.getEnv= function () {
+//        return $window.env;
+//      }
+//    
+//    this.saveUserName = function(username){
+//    	$window.localStorage.setItem('username', username);
+//    }
+//    this.getUserName = function(){
+//    	return $window.localStorage.getItem('username');
+//    }
+    var createUser = function(email, password){
+    var ref = new Firebase("https://stevetest1.firebaseio.com");
+        ref.createUser({
+            email    : "email",
+            password : "password"
+        }, function(error, userData) {
+        if (error) {
+        console.log("Error creating user:", error);
+        } else {
+        console.log("Successfully created user account with uid:", userData.uid);
+        }
+    });
+    }
     
-    this.saveUserName = function(username){
-    	$window.localStorage.setItem('username', username);
-    }
-    this.getUserName = function(){
-    	return $window.localStorage.getItem('username');
-    }
-
- //   var ref = new Firebase("https://stevetest1.firebaseio.com");
-	// 	ref.authWithPassword({
- //  			email    : "email",
- //  			password : "password"
-	// 	}, function(error, authData) {
- //  			if (error) {
- //   		 		console.log("Login Failed!", error);
- //  			} else {
- //    		console.log("Authenticated successfully with payload:", authData);
- //  			}
-	// });
+    
+    var ref = new Firebase("https://stevetest1.firebaseio.com");
+	 	ref.authWithPassword({
+   			email    : "email",
+   			password : "password"
+	 	}, function(error, authData) {
+   			if (error) {
+    		 		console.log("Login Failed!", error);
+   			} else {
+     		console.log("Authenticated successfully with payload:", authData);
+   			}
+	 });
 });
